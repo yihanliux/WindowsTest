c7de4455aacf966f95209439961584637b2641ed
yihanliux <yihanliu3@163.com>
2025-06-13 15:27:00 +0800

    push

--- .venv1/Lib/site-packages/rtmlib/tools/solution/__init__.py
+++
@@ -1,8 +0,0 @@
-from .body import Body
-from .hand import Hand
-from .pose_tracker import PoseTracker
-from .wholebody import Wholebody
-from .body_with_feet import BodyWithFeet
-from .custom import Custom
-
-__all__ = ['Wholebody', 'Body', 'PoseTracker', 'Hand', 'BodyWithFeet', 'Custom']
